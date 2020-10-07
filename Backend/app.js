b4d6fc9fd13194c2920226c5a1cc9c961299b896
yashvc738 <yash56244@gmail.com>
2020-10-07 13:07:38 +0530

    Added login required middleware

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const MONGOURI =
-  "mongodb+srv://Yash:ui2Soj2wR3hJOOqe@cluster0.ijyx1.mongodb.net/test?retryWrites=true&w=majority";
+    "mongodb+srv://Yash:ui2Soj2wR3hJOOqe@cluster0.ijyx1.mongodb.net/test?retryWrites=true&w=majority";
 const PORT = 5000;
 
 require("./models/user");
@@ -11,16 +11,16 @@ app.use(express.json());
 app.use(require("./routes/auth"));
 
 mongoose.connect(MONGOURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB");
 });
 mongoose.connection.on("error", (err) => {
-  console.log("ERROR : ", err);
+    console.log("ERROR : ", err);
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on", PORT);
+    console.log("Server is running on", PORT);
 });
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,64 +1,88 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = mongoose.model("User");
+const JWT_SECRET =
+    "$2a$16$M8S4d1xj/p3won8BzNn6OOrV/H5p44v5s.fPIsr7X.iKYapnoRyLC";
+
+const loginRequired = (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) {
+        return res.status(401).json({ error: "You must be logged in" });
+    }
+    const token = authorization.replace("Bearer ", "");
+    jwt.verify(token, JWT_SECRET, (err, payload) => {
+        if (err) {
+            return res.status(401).json({ error: err });
+        }
+        const { _id } = payload;
+        User.findById(_id).then((user) => {
+            req.user = user;
+        });
+        next();
+    });
+};
 
 router.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(422).json({ error: "All fields are required" });
-  }
-  User.findOne({ email: email })
-    .then((userExists) => {
-      if (userExists) {
-        return res
-          .status(422)
-          .json({ error: "User already exists with that email" });
-      }
-      bcrypt.hash(password, 16).then((hashedPassword) => {
-        const user = new User({
-          email,
-          password: hashedPassword,
-          name,
-        });
-        user
-          .save()
-          .then((user) => {
-            res.json({ message: "Account created successfully" });
-          })
-          .catch((err) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return res.status(422).json({ error: "All fields are required" });
+    }
+    User.findOne({ email: email })
+        .then((userExists) => {
+            if (userExists) {
+                return res
+                    .status(422)
+                    .json({ error: "User already exists with that email" });
+            }
+            bcrypt.hash(password, 16).then((hashedPassword) => {
+                const user = new User({
+                    email,
+                    password: hashedPassword,
+                    name,
+                });
+                user.save()
+                    .then((user) => {
+                        res.json({ message: "Account created successfully" });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            });
+        })
+        .catch((err) => {
             console.log(err);
-          });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+        });
 });
 
 router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(422).json({ error: "All fields are required" });
-  }
-  User.findOne({ email: email }).then((user) => {
-    if (!user) {
-      return res.status(422).json({ error: "Invalid Credentials!" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).json({ error: "All fields are required" });
     }
-    bcrypt
-      .compare(password, user.password)
-      .then((match) => {
-        if (match) {
-          res.json({ message: "Successfully Logged IN" });
-        } else {
-          return res.status(422).json({ error: "Invalid Credentials!" });
+    User.findOne({ email: email }).then((user) => {
+        if (!user) {
+            return res.status(422).json({ error: "Invalid Credentials!" });
         }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+        bcrypt
+            .compare(password, user.password)
+            .then((match) => {
+                if (match) {
+                    //   res.json({ message: "Successfully Logged IN" });
+                    const token = jwt.sign({ _id: user._id }, JWT_SECRET);
+                    res.send({ token });
+                } else {
+                    return res
+                        .status(422)
+                        .json({ error: "Invalid Credentials!" });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 });
 
 module.exports = router;
