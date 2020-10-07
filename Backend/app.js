ce1331dfd13bb4df1b29eec5991125b4a1e0f2ca
yashvc738 <yash56244@gmail.com>
2020-10-07 11:13:51 +0530

    Connected MongoDB

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const MONGOURI = "mongodb+srv://Yash:ui2Soj2wR3hJOOqe@cluster0.ijyx1.mongodb.net/test?retryWrites=true&w=majority";
+const PORT = 5000;
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', ()=>{
+    console.log("Connected to MongoDB");
+})
+mongoose.connection.on('error', (err)=>{
+    console.log("ERROR : ", err);
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server is running on", PORT);
+})
