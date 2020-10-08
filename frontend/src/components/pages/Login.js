abcecefe7ab6361f9fe6a4366b6429c4b08056af
yashvc738 <yash56244@gmail.com>
2020-10-08 18:00:35 +0530

    added login & register UI

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Login = ()=>{
+const Login = () => {
     return (
-        <h1>Login</h1>
-    )
-}
+        <div className="row center">
+            <div className="col card l6">
+                <h2>Instagram</h2>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input id="email" type="email" className="validate" />
+                        <label for="email">Email</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input
+                            id="password"
+                            type="password"
+                            className="validate"
+                        />
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+                <button
+                    className="btn waves-effect waves-light"
+                    type="submit"
+                    name="action"
+                >
+                    Login
+                </button>
+                <p>
+                    Don't have an account? <Link to="/register">Sign up</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export default Login;
--- frontend/src/components/pages/Register.js
+++ frontend/src/components/pages/Register.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Register = ()=>{
+const Register = () => {
     return (
-        <h1>Register</h1>
-    )
-}
+        <div className="row center">
+            <div className="col card l6">
+                <h2>Instagram</h2>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input id="name" type="text" className="validate" />
+                        <label for="name">Name</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input id="email" type="email" className="validate" />
+                        <label for="email">Email</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input
+                            id="password"
+                            type="password"
+                            className="validate"
+                        />
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+                <button
+                    className="btn waves-effect waves-light"
+                    type="submit"
+                    name="action"
+                >
+                    Register
+                </button>
+                <p>
+                Have an account? <Link to="/login">Log In</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export default Register;
