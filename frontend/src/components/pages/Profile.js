208157cac0e87248696653c0387f0f61e998ac4b
yashvc738 <yash56244@gmail.com>
2020-10-08 18:44:17 +0530

    added profile page UI

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,9 +1,79 @@
-import React from 'react';
+import React from "react";
 
-const Profile = ()=>{
+const Profile = () => {
     return (
-        <h1>Profile</h1>
-    )
-}
+        <div className="profile-page">
+            <div className="profile-container">
+                <div>
+                    <img
+                        className="profile-img"
+                        src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <h4>Yash Chaudhari</h4>
+                    <div className="profile-info">
+                        <h6>40 posts</h6>
+                        <h6>40 following</h6>
+                        <h6>40 followers</h6>
+                    </div>
+                </div>
+            </div>
+            <div className="profile-gallery">
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+                <img
+                    className="profile-gallery-img"
+                    src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt=""
+                />
+            </div>
+        </div>
+    );
+};
 
 export default Profile;
