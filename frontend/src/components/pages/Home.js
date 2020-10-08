3004cf530238c9b42d36fbbfdc46efb97c7260eb
yashvc738 <yash56244@gmail.com>
2020-10-08 22:02:27 +0530

    added home page UI

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const Home = ()=>{
+const Home = () => {
     return (
-        <h1>Home</h1>
-    )
-}
+        <div>
+            <div className="card home-card">
+                <h4><img src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" className="home-profile-img" alt="" />Yash Chaudhari</h4>
+                <div className="card-image">
+                    <img
+                        src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                        alt=""
+                    />
+                </div>
+                <div className="card-content">
+                    <h6>Caption</h6>
+                    <input type="text" placeholder="Add a Comment.." />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Home;
